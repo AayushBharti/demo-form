3f640fc0d1cab336eae62ee2ae006b88acc60897
Aayush Bharti <aayushbh2002@gmail.com>
2024-03-30 15:50:31 +0530

    update

--- src/App.js
+++ src/App.js
@@ -33,13 +33,13 @@ function App() {
   }
 
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center justify-center">
       <form
         onSubmit={submitHandler}
-        className="max-w-[800px] w-full mt-2 p-4 border-2 border-blue-400 
-        rounded-lg space-y-1"
+        className="max-w-[800px] w-full my-16 p-5 border-2 border-blue-400 
+        rounded-lg space-y-1.5"
       >
-        <label htmlFor="firstName">First Name</label>
+        <label htmlFor="firstName" className="font-semibold">First Name</label>
         <br />
         <input
           type="text"
@@ -48,10 +48,10 @@ function App() {
           placeholder="Aayush"
           value={formData.firstName}
           onChange={changeHandler}
-          className="border border-black rounded-md w-full px-3 py-1"
+          className="border border-blue-950 rounded-md w-full px-3 py-1"
         />
 
-        <label htmlFor="lastName">Last Name</label>
+        <label htmlFor="lastName" className="font-semibold">Last Name</label>
         <br />
         <input
           type="text"
@@ -60,10 +60,10 @@ function App() {
           placeholder="Bharti"
           value={formData.lastName}
           onChange={changeHandler}
-          className="border border-black rounded-md w-full px-3 py-1"
+          className="border border-blue-950 rounded-md w-full px-3 py-1"
         />
 
-        <label htmlFor="email">E-mail</label>
+        <label htmlFor="email" className="font-semibold">E-mail</label>
         <br />
         <input
           type="email"
@@ -72,17 +72,17 @@ function App() {
           placeholder="iaayushbharti@gmail.com"
           value={formData.email}
           onChange={changeHandler}
-          className="border border-black rounded-md w-full px-3 py-1"
+          className="border border-blue-950 rounded-md w-full px-3 py-1"
         />
 
-        <label htmlFor="country">Country</label>
+        <label htmlFor="country" className="font-semibold">Country</label>
         <br />
         <select
           name="country"
           id="country"
           value={formData.country}
           onChange={changeHandler}
-          className="border border-black rounded-md w-full px-3 py-1"
+          className="border border-blue-950 bg-white rounded-md w-full px-3 py-1"
         >
           <option value="india">India</option>
           <option value="usa">USA</option>
@@ -90,7 +90,7 @@ function App() {
           <option value="mexico">Mexico</option>
         </select>
 
-        <label htmlFor="streetAddress">Street Address</label>
+        <label htmlFor="streetAddress" className="font-semibold">Street Address</label>
         <br />
         <input
           type="text"
@@ -99,10 +99,10 @@ function App() {
           placeholder="Indira Nagar"
           value={formData.streetAddress}
           onChange={changeHandler}
-          className="border border-black rounded-md w-full px-3 py-1"
+          className="border border-blue-950 rounded-md w-full px-3 py-1"
         />
 
-        <label htmlFor="city">City</label>
+        <label htmlFor="city" className="font-semibold">City</label>
         <br />
         <input
           type="text"
@@ -111,10 +111,10 @@ function App() {
           placeholder="Patna"
           value={formData.city}
           onChange={changeHandler}
-          className="border border-black rounded-md w-full px-3 py-1"
+          className="border border-blue-950 rounded-md w-full px-3 py-1"
         />
 
-        <label htmlFor="state">State / Province</label>
+        <label htmlFor="state" className="font-semibold">State / Province</label>
         <br />
         <input
           type="text"
@@ -123,10 +123,10 @@ function App() {
           placeholder="Bihar"
           value={formData.state}
           onChange={changeHandler}
-          className="border border-black rounded-md w-full px-3 py-1"
+          className="border border-blue-950 rounded-md w-full px-3 py-1"
         />
 
-        <label htmlFor="postalCode">ZIP / Postal Code</label>
+        <label htmlFor="postalCode" className="font-semibold">ZIP / Postal Code</label>
         <br />
         <input
           type="number"
@@ -135,11 +135,11 @@ function App() {
           placeholder="800001"
           value={formData.postalCode}
           onChange={changeHandler}
-          className="border border-black rounded-md w-full px-3 py-1"
+          className="border border-blue-950 rounded-md w-full px-3 py-1"
         />
 
         <fieldset>
-          <legend className="mt-3">By Email</legend>
+          <legend className="mt-3 font-semibold">By Email</legend>
 
           <input
             type="checkbox"
@@ -201,7 +201,7 @@ function App() {
             onChange={changeHandler}
             className="my-2"
           />
-          <label htmlFor="pushEverything">Everything</label>
+          <label htmlFor="pushEverything" className="ml-1">Everything</label>
           <br />
 
           <input
@@ -212,7 +212,7 @@ function App() {
             onChange={changeHandler}
             className="my-2"
           />
-          <label htmlFor="pushEmail">Same as email</label>
+          <label htmlFor="pushEmail" className="ml-1">Same as email</label>
           <br />
 
           <input
@@ -223,7 +223,7 @@ function App() {
             onChange={changeHandler}
             className="my-2"
           />
-          <label htmlFor="pushNothing">No Push Notifications</label>
+          <label htmlFor="pushNothing" className="ml-1">No Push Notifications</label>
           <br />
         </fieldset>
 
